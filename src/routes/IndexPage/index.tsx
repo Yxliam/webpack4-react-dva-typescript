c1c0bbaf3d32bb2eea7f3f844852a66ce808fd59
seaseeyoul <seaseeyoul@qq.com>
2019-01-21 12:02:50 +0800

    一个ts版本的dva-cli

---
+++ src/routes/IndexPage/index.tsx
@@ -0,0 +1,10 @@
+import React,{ SFC } from 'react';
+import styles from './index.module.scss';
+
+// 无状态
+
+const IndexPage:SFC = () =>(
+    <div className={ styles.title }>hello,react-dva-clgggi</div>
+)
+
+  export default IndexPage;
